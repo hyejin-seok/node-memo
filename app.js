d93bfa0e60d1e7df73f20fe11cec340e087ee3c2
Hyejin Seok <hyejin.seok7@gmail.com>
2024-01-21 15:14:04 -0800

    work on auth

--- app.js
+++ app.js
@@ -1,31 +1,48 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const PORT = 3002;
 
 const memoRoutes = require("./routes/memoRoutes");
-const sercureKey = "adfqef1233afdgadf";
-const users = {
-  admin: "admin11",
-  guest: "guest22",
-};
 
-app.use(cookieParser(sercureKey));
-app.use(express.json());
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+const users = [];
+const sercureKey = "adfqef1233afdgadf";
 
 app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  if (req.cookies && req.cookies.userCookie) {
-    res.render("index", { pageTitle: "Home", user: req.cookies.userCookie });
-  } else {
-    res.redirect("/login");
+app.use(cookieParser(sercureKey));
+app.use(express.static("public"));
+
+const checkAuthenticated = (req, res, next) => {
+  if (req.cookies.userCookie) {
+    return next(); // User is authenticated
   }
+  res.redirect("/login");
+};
+
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index", { pageTitle: "Home", user: req.cookies.userCookie });
 });
 
-app.use("/memos", memoRoutes);
+app.get("/signup", (req, res) => {
+  res.render("signup", { pageTitle: "Sign up" });
+});
+
+app.post("/signup", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      username: req.body.username.toLowerCase(),
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+  } catch (error) {
+    res.redirect("/signup");
+  }
+});
 
 app.get("/login", (req, res) => {
   const { error, logout } = req.query;
@@ -36,18 +53,38 @@ app.get("/login", (req, res) => {
   }
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
+  console.log("Login Request Received");
   if (req.cookies && req.cookies.userCookie) {
+    console.log("User is already authenticated");
     res.redirect("/");
   } else {
     const { username, password } = req.body;
-    if (users[username] && users[username] === password) {
-      res.cookie("userCookie", username, {
-        httpOnly: true,
-        maxAge: 60000 * 60, // 1hr
-      });
-      res.redirect("/");
+    console.log("Entered username:", username);
+    console.log("Entered password:", password);
+
+    const user = users.find(
+      (u) => u.username.toLowerCase() === username.toLowerCase()
+    );
+    console.log("Found user:", user);
+
+    if (user) {
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      console.log("Password match result:", passwordMatch);
+
+      if (passwordMatch) {
+        console.log("Authentication successful");
+        res.cookie("userCookie", username, {
+          httpOnly: true,
+          maxAge: 60000 * 60, // 1hr
+        });
+        res.redirect("/");
+      } else {
+        console.log("Password does not match");
+        res.redirect("/login?error=1");
+      }
     } else {
+      console.log("User not found");
       res.redirect("/login?error=1");
     }
   }
@@ -58,11 +95,8 @@ app.get("/logout", (req, res) => {
   res.redirect("/login?logout=success");
 });
 
-app.get("/clear-cookies", (req, res) => {
-  res.clearCookie("firstCookie");
-  res.send("Cookie deleted!");
-});
+app.use("/memos", checkAuthenticated, memoRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}..:)`);
+  console.log(`Server is running on port ${PORT}..`);
 });
