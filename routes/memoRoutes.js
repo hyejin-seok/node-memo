161828c4176b33735d91f754316bbde8f7a4dd60
Hyejin Seok <hyejin.seok7@gmail.com>
2024-01-21 19:27:45 -0800

    work on logic

--- routes/memoRoutes.js
+++ routes/memoRoutes.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 const memoController = require("../controllers/memoController");
 
+// Route to add a memo
+router.get("/add", (req, res) => {
+  console.log("Reached GET /memos/add");
+  res.render("memos/addMemo", { pageTitle: "Create Memo" });
+});
+
 // Route to fetch all memos
 router.get("/", (req, res) => {
   const memos = memoController.getAllMemos();
-  res.render("memos", { memos, pageTitle: "Memos List" });
+  res.render("memos/memos", { memos, pageTitle: "Memos List" });
   //   res.json(memos);
 });
 
@@ -14,27 +20,40 @@ router.get("/:id", (req, res) => {
   const memoId = parseInt(req.params.id); // 여기서 id의 type은 string이라서 숫자로 변환 필요.
   const memo = memoController.getMemoById(memoId);
   if (!memo) {
-    res.status(404).json({ error: "No memo exist" });
+    res.status(404).json({ error: "Memo not found" });
   } else {
-    res.status(200).json(memo);
+    // res.status(200).json(memo);
+    res.render("memos/memoDetails", { memo, pageTitle: "Memo Details" });
   }
 });
 
-// Route to add a memo
-router.post("/", (req, res) => {
-  const { title, content } = req.body; // req body is from POST request body
+router.post("/add", (req, res) => {
+  console.log("Reached POST /memos/add");
+  const { title, content } = req.body;
   const memo = memoController.createMemo(title, content);
-  res.status(201).json(memo); // status code for creating, adding is 201 (status code is optional)
+  res.redirect("/memos");
 });
-// Route to update memo
-router.put("/:id", (req, res) => {
+
+// Route to edit memo (render the form)
+router.get("/:id/edit", (req, res) => {
+  const memoId = parseInt(req.params.id);
+  const memo = memoController.getMemoById(memoId);
+  if (!memo) {
+    res.status(404).json({ error: "Memo not found" });
+  } else {
+    res.render("memos/editMemo", { memo, pageTitle: "Edit Memo" });
+  }
+});
+
+// Route to handle form submission for editing memo
+router.post("/:id/edit", (req, res) => {
   const memoId = parseInt(req.params.id);
   const { title, content } = req.body;
   const memo = memoController.updateMemo(memoId, title, content);
   if (!memo) {
-    res.status(404).json({ error: "memo does not exist" });
+    res.status(404).json({ error: "Memo not found" });
   } else {
-    res.status(200).json(memo);
+    res.redirect("/memos");
   }
 });
 
@@ -43,7 +62,7 @@ router.delete("/:id", (req, res) => {
   const memoId = parseInt(req.params.id);
   const result = memoController.deleteMemo(memoId);
   if (result) {
-    res.status(200).json({ message: "Memo deleted!" });
+    res.redirect("/memos");
   } else {
     res.status(404).json({ error: "Memo does not exist" });
   }
